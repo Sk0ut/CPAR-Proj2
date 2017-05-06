cb27132f6a6398f0af33b740a57cb1d11cb83e53
carlosamouco <carlosamouco@gmail.com>
2017-05-06 12:39:11 -0700

    added missing fike

---
+++ sieve.cpp
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+#include <string.h>
+#include <stdlib.h>
+#include <vector>
+#include <omp.h>
+
+using namespace std;
+
+//	#include "mpi.h"
+
+void printProcessingTime(double timeMs) 
+{
+	char st[100];
+
+	sprintf(st, "Time: %3.3f seconds\n", timeMs);	
+	printf("%s", st);
+}
+
+void sequentialSieveParallel(uint64_t n)
+{       
+  double Time1, Time2;
+
+  Time1 = omp_get_wtime();
+
+  uint32_t size = n / 2 + n % 2;
+  uint32_t max_seed_index = sqrt(n) / 2;  
+  vector<bool> marked(size);
+
+  for (uint32_t i = 0; i < max_seed_index ; ++i) {
+    if (!marked[i]) {
+      uint32_t value = 2 * i + 3;      
+      uint32_t maxFactor = (uint32_t) n / value;
+      #pragma omp parallel for num_threads(4)
+      for (uint64_t j = value ; j <= maxFactor; j += 2) {
+          marked[(j*value-3) / 2] = 1;
+      }
+    }
+  }
+
+  Time2 = omp_get_wtime();	
+	double processingTime = Time2 - Time1;
+
+  printProcessingTime(processingTime);
+  
+  int count = 1;
+
+  printf("Prime numbers till %ld:", n);
+	printf(" %d", 2);
+  for (uint32_t i = 0; i < size - 1; ++i) {
+    if (!marked[i]) {
+      printf(" %u", 2*i + 3);
+      ++count;
+    }
+  }
+  printf("\n");
+  printf("Total Primes: %d\n",count);
+}
+
+void sequentialSieve(uint64_t n)
+{       
+  double Time1, Time2;
+
+  Time1 = omp_get_wtime();
+
+  uint32_t size = n / 2 + n % 2;
+  uint32_t max_seed_index = sqrt(n) / 2;  
+  vector<bool> marked(size);
+
+  for (uint32_t i = 0; i < max_seed_index ; ++i) {
+    if (!marked[i]) {
+      uint32_t value = 2 * i + 3;
+      for (uint64_t j = value * value; j <= n; j += 2*value) {
+          marked[(j-3) / 2] = 1;
+      }
+    }
+  }
+
+  Time2 = omp_get_wtime();	
+	double processingTime = Time2 - Time1;
+
+  printProcessingTime(processingTime);
+
+  /*printf("Prime numbers till %ld:", n);
+	printf(" %d", 2);
+  for (uint32_t i = 0; i < size - 1; ++i) {
+    if (!marked[i]) {
+      printf(" %u", 2*i + 3);
+    }
+  }
+  printf("\n");*/
+}
+
+
+int main (int argc, char *argv[])
+{
+	if (argc != 2) 
+	{
+		printf("Usage: %s <n>\n", argv[0]);
+	}
+	else
+	{
+		uint32_t n = atoi(argv[1]);
+		if (n < 2)
+		{
+			printf("n must be grater or equal than 2.");
+		}
+		else
+		{
+			sequentialSieveParallel(n);
+		}
+	}
+	return 0;
+}
