1d5d48536e84d6c560a62b377dd87fc769808f39
carlosamouco <carlosamouco@gmail.com>
2017-05-10 20:45:56 -0700

    added func body [incomplete]

--- OpenMPISieve.cpp
+++ OpenMPISieve.cpp
@@ -2,13 +2,26 @@
 #include <mpi.h>
 #include <iostream>
 
+
+using namespace std;
+
+void openMPISieve(uint64_t n) {
+    int rank, size;
+    
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);    
+
+    cout << "Status: size=" << size << " rank=" << rank << endl;
+}
+
+
 int main(int argc, char** argv) {
 	MPI_Init(&argc, &argv);
 
-	uint32_t n;
-	sscanf(argv[1], "%u", &n);
+	uint64_t n;
+	sscanf(argv[1], "%lu", &n);
 
-	//  openMPISieve(n);
+	openMPISieve(n);
 
 	MPI_Finalize();
 	return 0;
